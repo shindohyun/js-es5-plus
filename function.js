b2199bd57a9e864bb72c445d36d8977d8fb43ba2
dohyun <shindohyunx@gmail.com>
2022-04-14 16:01:39 +0900

    [추가] function 마무리

--- function.js
+++ function.js
@@ -46,6 +46,124 @@ function printAll(...args) {
     for(let i = 0; i < args.length; i++){
         console.log(args[i]);
     }
+
+    for(const arg of args) {
+      console.log(arg);
+    }
+
+    args.forEach((arg) => console.log(arg));
 }
 
 printAll('dream', 'coding', 'ellie');
+
+//-------------------------------------------------------
+
+// 5. Local Scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+  let message = 'hello';
+  console.log(message); // local variable
+  console.log(globalMessage);
+  function printAnother() {
+    console.log(message);
+    let childMessage = 'hello';
+  }
+  // console.log(childMessage); // error
+  // return undefined;
+}
+printMessage();
+
+//-------------------------------------------------------
+
+// 6. Return a value
+function sum(a, b) {
+  return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+//-------------------------------------------------------
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+  if(user.point > 10) {
+    // long upgrade logic...
+  }
+}
+
+// good
+function upgradeUser(user) {
+  if(user.point <= 10) {
+    return;
+  }
+  // long upgrade logic...
+}
+
+// First-class function
+// functions are treated like any other variable
+// can be assigned as a value to variable
+// can be passed as an argument to other functions.
+// can be returned by another function
+
+// 1. Function expression
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+// function expression
+// print(); // error
+const print = function() { // anonymous function
+  console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// function declaration
+print2();
+function print2() {
+  console.log('print2');
+};
+
+// 2. Callback function using function expression
+function randomQuiz(answer, printYes, printNo) {
+  if(answer === 'love you') {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+// anonymous function
+const printYes = function () {
+  console.log('yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print() {
+  console.log('no!');
+  // print();
+};
+
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+const simplePrint1 = function () {
+  console.log('simplePrint!');
+};
+
+const simplePrint2 = () => console.log('simplePrint!');
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+  // do something more
+  return a * b;
+};
+
+// IIFE: Immediately Invoked Function Expression
+(function hello() {
+  console.log('IIFE');
+})();
